6802e48a597089a87c677b8480b4458384788ea3
Swastik <swastikpanda32@gmail.com>
2024-03-12 23:45:02 +0530

    completed top k freq elements and product of arr except self

---
+++ Arrays-Hashing/TopKfrequentElement.js
@@ -0,0 +1,55 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+
+// solution
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  let map = {};
+  let bucket = [];
+  let result = [];
+
+  // add map with nums and its respective freq
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else {
+      map[nums[i]]++;
+    }
+  }
+
+  // check if the nums exist for their respective freq
+  for (let [nums, freq] of Object.entries(map)) {
+    if (!bucket[freq]) {
+      bucket[freq] = new Set().add(nums);
+    } else {
+      bucket[freq] = bucket[freq].add(nums);
+    }
+  }
+
+  // start traversing the array from backwards as most frequencies are at the end
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i]);
+    if (result.length === k) break;
+  }
+  return result;
+};
